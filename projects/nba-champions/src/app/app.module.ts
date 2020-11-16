439c4d62f7e769a60f173ea578a4faf0d47ea0df
Prodan <proboy599@gmail.com>
2020-11-16 09:01:00 +0100

    done

---
+++ projects/nba-champions/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChampionsComponent } from './champions/champions.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChampionsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
